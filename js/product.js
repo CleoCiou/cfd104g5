a0372953939faf561457e0d2d30dedc64346b868
AnnieLu1101 <m10736002@gemail.yuntech.edu.tw>
2022-02-27 19:57:44 +0800

    增加歡迎頁 修改首頁 商品頁 產品內頁 還須更改 已放入heart footer 並移至html資料夾內

--- js/product.js
+++ js/product.js
@@ -1,3 +1,4 @@
+// 點小圖秀大圖
 function showLarge(e){
   let small = e.target;
   document.getElementById("large").src = small.src ;
@@ -11,3 +12,20 @@ function init(){
 }
 
 window.addEventListener("load", init, false);
+
+// 推薦商品 貓頭鷹套件
+
+$(".owl-carousel").owlCarousel({
+    loop: true, // 循環播放
+    margin: 10, // 外距 10px
+    dots: false,
+    nav: true, // 顯示點點
+    responsive: {
+        0: {
+            items: 1 // 螢幕大小為 0~600 顯示 1 個項目
+        },
+        980: {
+            items: 3 // 螢幕大小為 1000 以上 顯示 5 個項目
+        }
+    }
+});
