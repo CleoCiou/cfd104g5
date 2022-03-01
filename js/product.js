2170e4341743519b2567fb2489fd86827b9fedd8
AnnieLu1101 <m10736002@gemail.yuntech.edu.tw>
2022-03-02 07:47:48 +0800

    首頁更新 產品更新 商城更新 咻~

--- js/product.js
+++ js/product.js
@@ -1,31 +1,33 @@
 // 點小圖秀大圖
 function showLarge(e){
-  let small = e.target;
-  document.getElementById("large").src = small.src ;
-}
-
-function init(){
-	let smalls = document.getElementsByClassName("small");
-	for(let i=0; i<smalls.length; i++){
-		smalls[i].onclick = showLarge;
-	}
-}
-
-window.addEventListener("load", init, false);
+    let small = e.target;
+    document.getElementById("large").src = small.src ;
+  }
+  
+  function init(){
+      let smalls = document.getElementsByClassName("small");
+      for(let i=0; i<smalls.length; i++){
+          smalls[i].onclick = showLarge;
+      }
+  }
+  
+  window.addEventListener("load", init, false);
 
 // 推薦商品 貓頭鷹套件
 
 $(".owl-carousel").owlCarousel({
     loop: true, // 循環播放
     margin: 10, // 外距 10px
-    dots: false,
-    nav: true, // 顯示點點
+    dots: true,
+    nav: false, // 顯示點點
     responsive: {
         0: {
-            items: 1 // 螢幕大小為 0~600 顯示 1 個項目
+            items: 2 // 螢幕大小為 0~600 顯示 1 個項目
         },
         980: {
-            items: 3 // 螢幕大小為 1000 以上 顯示 5 個項目
+            items: 5 // 螢幕大小為 1000 以上 顯示 5 個項目
         }
     }
 });
+
+
