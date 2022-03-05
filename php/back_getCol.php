94922fadfd3613880ed3cfd030d4c65fa2e289b2
CleoCiou <a0930795302@gmail.com>
2022-03-05 21:47:55 +0800

    後台串接

--- php/back_getCol.php
+++ php/back_getCol.php
@@ -3,20 +3,33 @@
     header('Content-Type: application/json; charset=utf-8');
     
     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (!isset($_POST['table']) || !isset($_POST['queryCol'])) {
+            $msg['msg'] = '資料表或欄位未設定';
+            echo json_encode($msg);
+            return;
+        }
+        
         // 查詢資料表
         $table = $_POST['table'];
-        // 查詢欄位
         $queryCol = $_POST['queryCol'];
+        $sql = "select $queryCol FROM $table";
+        $msg['sql'] = $sql;
+
+
+        // 如果有JOIN
+        if (isset($_POST['joinTable']) && isset($_POST['joinOn'])) {
+            $joinTable = $_POST['joinTable'];
+            $joinOn = $_POST['joinOn'];
+            $joinSql = "$joinTable on ($table.$joinOn = $joinTable.$joinOn)";
+            $msg['sql']="$sql join $joinSql";
+            $sql = "$sql join $joinSql";
+        }
         
         try {
-            // $query="select ? from ?";
-            // $data = [$queryCol, $table];
-            // $result=$pdo->prepare($query);
-            // $result->execute($data);
-            $result = $pdo -> query("select $queryCol FROM $table");
+            $result = $pdo -> query($sql);
             $count = $result->rowCount();
             
-            if ($count === 1) {
+            if ($count > 0) {
                 $msg['msg'] = $result -> fetchAll(PDO::FETCH_ASSOC);
             }
             else {
