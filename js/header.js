785f72e83014a1fe5fe518ab1e60da932e7301a2
En-Chieh <angel84280@gmail.com>
2022-03-05 20:24:33 +0800

    微調

--- js/header.js
+++ js/header.js
@@ -3,4 +3,11 @@ $(function() {
         // $('.sub_menu').slideUp();
         $(this).siblings('header .sub_menu').slideToggle();
     })
+    $('header .menu_link').click(function(){
+        $(this).toggleClass("click_btn");
+    })
+    $('.sub_menu li a').click(function(){
+        $(this).toggleClass("click_sub_menu");
+    })
+s
 })
