9a55dbe5ff6278e7fc831d8f23eb1363852d23da
jctsai104 <alice60313@hotmail.com>
2022-03-06 01:28:59 +0800

    改header的scroll 星座兩頁+切換和微調(還沒改完Q_Q vue好難) 有玩一下資料庫跟axios

--- js/header.js
+++ js/header.js
@@ -4,3 +4,28 @@ $(function() {
         $(this).siblings('header .sub_menu').slideToggle();
     })
 })
+
+// header縮上去的transition
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 0) {
+        $('#mainHeader').addClass('nav_animation');
+    } else {
+        $('#mainHeader').removeClass('nav_animation');
+    }
+});
+
+// 滑鼠滾輪控制header的伸縮
+function doFirst(){
+    let prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("mainHeader").style.top = "0";
+        } else {
+            document.getElementById("mainHeader").style.top = "-75px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
+
+window.addEventListener('load', doFirst);
