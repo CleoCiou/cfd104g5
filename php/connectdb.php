78e15ac4dd75b163f25c4e9ffb13e2746f1ca8d5
CleoCiou <a0930795302@gmail.com>
2022-03-06 16:25:03 +0800

    更改資料庫連線參數

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,11 +1,12 @@
 <?php
     try {
         // 資料庫設定
+        $host = "cfd104g5.asuscomm.com";
         $dbname = "g5";
-        $user = "root";
-        $password = "00000";
+        $user = "pin"; // 記得改成自己的名字
+        $password = "cfd104_g5@gogo";
     
-        $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
+        $dsn = "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8";
     
         $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     
