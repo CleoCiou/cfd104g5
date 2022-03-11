2700f7119e774407cbad3680f827f85be4a4a18b
CleoCiou <a0930795302@gmail.com>
2022-03-11 20:34:07 +0800

    更新back_getCol為第二種較為安全的查詢方式 更新後台帳號管理、會員管理、商品管理查詢方式 更新會員專區，個人資料撈資料庫的值 更新原common.js，新增取得登入後儲存的session資料 新增getSessionData.php，取得已儲存的Session資料 新增check_login.php，進行登入判斷且將資訊存入session

---
+++ php/check_login.php
@@ -0,0 +1,49 @@
+<?php
+    include('connectdb.php');
+    
+    session_start();
+    
+    // 登入帳號密碼檢查
+    if (isset($_REQUEST['id']) && isset($_REQUEST['pwd'])) {
+        $data=[$_REQUEST['id'], $_REQUEST['pwd']];
+        
+        $sql = "select * from members where memId = ? and memPwd = ?";
+
+        try {
+            $result = $pdo -> prepare($sql);
+            $result -> execute($data);
+    
+            $count = $result->rowCount();
+            
+            // 帳號密碼正確
+            if ($count > 0) {
+                $row = $result -> fetch(PDO::FETCH_ASSOC);
+                
+                if ($row['status'] == '停權') {
+                    $msg['msg'] = '您的帳號已被停權';
+                    return;
+                }
+
+                // 登入成功
+                $msg['msg'] = true;
+                $_SESSION['userId'] = $row['memId'];
+                $_SESSION['userName'] = $row['memName'];
+                $_SESSION['identity'] = $row['identity'];
+                $_SESSION['sticker'] = $row['memImage'];
+                $_SESSION['email'] = $row['email'];
+                $_SESSION['creditNum'] = $row['creditNum'];
+                // ...
+
+            }
+            // 登入失敗
+            else {
+                $msg['msg'] = '帳號或密碼錯誤';
+            }
+        }
+        catch (PDOException $e){
+            $msg['msg'] = $e -> getMessage();
+        }    
+    }
+
+    echo json_encode($msg);
+?>
