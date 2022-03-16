4fef67283ebce6c0a0952bd0e85e4a7992680b85
jjameshao00 <smile2330895@gmail.com>
2022-03-16 16:14:47 +0800

    discuss撈資料OK

---
+++ phps/post.php
@@ -0,0 +1,48 @@
+<?php
+    // 資料庫連線檔
+    // 記得去改帳號
+    include('connectdb.php');
+
+    // header 格式
+    header('Content-Type: application/json; charset=utf-8');
+    
+    // 如果 ajax 使用 POST 傳送才處理
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        switch ($_POST["case"]) {
+
+            case "hotPost":
+
+                $sql = "SELECT artno, article.topicNo, topicName, article.memNo, members.memName, title, artTime, content, likes, artFavs FROM `article` 
+                join members on members.memNo = article.memNo
+                join article_topic on article.topicNo = article_topic.topicNo";
+
+            
+            
+                $new = $pdo->query($sql);
+                $newRows = $new->fetchAll(PDO::FETCH_ASSOC);
+            
+                echo json_encode($newRows);
+                    
+                break;
+        
+            case "newPost":
+                $sql = "SELECT artno, article.topicNo, topicName, article.memNo, members.memName, title, artTime, content, likes, artFavs FROM `article` 
+                join members on members.memNo = article.memNo
+                join article_topic on article.topicNo = article_topic.topicNo
+                order by artTime desc
+                ";
+        
+                // 執行
+                $new = $pdo->query($sql);
+                $newRows = $new->fetchAll(PDO::FETCH_ASSOC);
+            
+                echo json_encode($newRows);
+                break;
+        
+    
+        }
+
+    }
+    
+?>
