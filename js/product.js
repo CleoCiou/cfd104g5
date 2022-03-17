1030c141bef48cf34da34d6b2a26aad121150572
AnnieLu1101 <m10736002@gemail.yuntech.edu.tw>
2022-03-18 00:47:02 +0800

    商城按btn可以分類了~感動得斯 商品照片路徑更動 把照片通通放在一個資料夾裡 星座資料夾刪除

--- js/product.js
+++ js/product.js
@@ -11,30 +11,13 @@ function init(){
     }
 }
   
-  window.addEventListener("load", init, false);
-
-// 推薦商品 貓頭鷹套件
-
-// $(document).on('ready',function(){
-    
-// });
-// $(".owl-carousel").owlCarousel({
-//     loop: true, // 循環播放
-//     margin: 10, // 外距 10px
-//     dots: true,
-//     nav: false, // 顯示點點
-//     responsive: {
-//         0: {
-//             items: 2 // 螢幕大小為 0~600 顯示 1 個項目
-//         },
-//         980: {
-//             items: 5 // 螢幕大小為 1000 以上 顯示 5 個項目
-//         }
-//     }
-// });
+window.addEventListener("load", init, false);
 
 
-// 撈資料哩
+
+
+// 撈資料哩 part1 
+// 下面還有part2哦
 function getProduct() {
     $.ajax({
         type: 'POST',
@@ -44,7 +27,7 @@ function getProduct() {
 
             table: 'product',
             joinTable: 'product_category',
-            queryCol: 'product.prodImage1 , product.prodImage2, product.prodImage3,product_category.cateType, product_category.cateName, product.prodName,product.price, product.prodspec, product.prodIntro,product.prodNo',
+            queryCol: 'product.prodImage1 , product.prodImage2, product.prodImage3,product_category.cateType, product_category.cateName, product.prodName,product.price, product.prodspec, product.prodIntro, product.prodNo',
             joinOn: 'prodCateNo',
             condition: `product.prodNo=${localStorage.getItem('prodNumber')}`,
         },
@@ -52,8 +35,9 @@ function getProduct() {
         success: function(data) {
             if (data.msg !== false){
                 console.log(data.msg);
-                // 照片路徑變數  
-                let productImg = "images/product_inner_page/tarot/wristband/";
+                // 照片路徑變數 手鍊  
+                let productImg = "images/shop/tarot/";
+                
                 // -要包最外層的div-
                 // 左邊點小圖秀大圖
                 let productRow =  `
@@ -97,23 +81,90 @@ function getProduct() {
                     <p>${data.msg[0].prodIntro}</p> 
                     `;
 
-                // 推薦商品
-                // let owlTheme = `<div class=" owl-theme ">`;
+                // 貓頭鷹推薦商品
+                
+                // let owlTheme = `<div class="owl-carousel owl-theme ">`;
+                // for (let i = 0; i < data.msg.length; i++) {
+
+                //     owlTheme +=   `
+                //         <a href="#">
+                //             <img src="${productImg}${data.msg[i].prodImage1}" alt="推薦商品" ">
+                //             <p>${data.msg[i].cateType}${data.msg[i].cateName} - ${data.msg[i].prodName}</p>
+                //         </a>
+                //     `;
+                // }
+                // owlTheme += `</div>`;
+
+                $('.product_inner_img_left').append(productRow);
+                $('.product_txt').append(productTxt);
+                $('.product_detail_txt').append(productDetail);
+                // $('.recommended_products_img').append(owlTheme);
+
+                // $(".owl-carousel").owlCarousel({
+                //     loop: true, // 循環播放
+                //     margin: 10, // 外距 10px
+                //     dots: true,
+                //     nav: false, // 顯示點點
+                //     responsive: {
+                //         0: {
+                //             items: 2 // 螢幕大小為 0~600 顯示 1 個項目
+                //         },
+                //         980: {
+                //             items: 5 // 螢幕大小為 1000 以上 顯示 5 個項目
+                //         }
+                //     }
+                // });
+
+                // 小圖換大圖
+                let smalls = document.getElementsByClassName("small");
+                for(let i=0; i<smalls.length; i++){
+                    smalls[i].onclick = showLarge;
+                }
+                
+            }
+        },
+        error: function() {
+            console.log('ajax error');
+        }
+    });
+}
+window.addEventListener('load', getProduct);
+
+
+// 撈推薦商品哩 part2 
+// 因為上面part1商品變數已設定為單筆localStorage 
+// 所以要重新再設一個變數給推薦商品
+
+function getRecommended() {
+    $.ajax({
+        type: 'POST',
+        url: 'phps/select.php',
+        
+        data: {
+            table: 'product',
+            joinTable: 'product_category',
+            queryCol: 'product.prodImage1 , product_category.cateType, product_category.cateName, product.prodName,  product.prodNo',
+            joinOn: 'prodCateNo',
+            condition:"product_category.prodCateNo = 1001",
+        },
+
+        success: function(data) {
+            if (data.msg !== false){
+                console.log(data.msg);
+                let recProductImg = "images/shop/tarot/";
                 let owlTheme = `<div class="owl-carousel owl-theme ">`;
                 for (let i = 0; i < data.msg.length; i++) {
 
                     owlTheme +=   `
-                        <a href="#">
-                            <img src="${productImg}${data.msg[i].prodImage1}" alt="推薦商品" ">
+                        <a class="item">
+                            <div class="prod_No">${data.msg[i].prodNo}</div>
+                            <img src="${recProductImg}${data.msg[i].prodImage1}" alt="推薦商品" ">
                             <p>${data.msg[i].cateType}${data.msg[i].cateName} - ${data.msg[i].prodName}</p>
                         </a>
                     `;
                 }
                 owlTheme += `</div>`;
 
-                $('.product_inner_img_left').append(productRow);
-                $('.product_txt').append(productTxt);
-                $('.product_detail_txt').append(productDetail);
                 $('.recommended_products_img').append(owlTheme);
 
                 $(".owl-carousel").owlCarousel({
@@ -131,17 +182,21 @@ function getProduct() {
                     }
                 });
 
-                // 小圖換大圖
-                let smalls = document.getElementsByClassName("small");
-                for(let i=0; i<smalls.length; i++){
-                    smalls[i].onclick = showLarge;
-                }
                 
+
             }
         },
         error: function() {
             console.log('ajax error');
         }
     });
+
 }
-window.addEventListener('load', getProduct);
+// 商城切換到商品頁面使用click事件
+$(document).on('click','.item',function(){
+    // alert($(this).find('.prod_No').text());
+    localStorage.setItem('prodNumber',$(this).find('.prod_No').text());
+    window.location.href = "product.html";
+});
+window.addEventListener('load', getRecommended);
+
